14caa7e8434b5cf33d3644cd261921068acb59cd
Martin Trenouth <mtrenouth@highperformancedata.com>
2017-08-22 00:13:02 -0700

    Email Service

---
+++ HPD.Utiltities/HPD.Utilities/SendGrid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace HPD.Utilities
+{
+    public class SendGridEmailService : Microsoft.AspNet.Identity.IIdentityMessageService
+    {
+        public  static string APIKey { get { return "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"; } }
+        public static string DefaultFromAddress { get; set; }
+
+        static async Task SendMessage(
+                string ToAddress,
+                string FromAddress,
+                string Subject,
+                string BodyHtml,
+                string BodyPlainText = null
+            )
+        {
+            var client = new SendGridClient(APIKey);
+            var from = new EmailAddress(FromAddress);
+            var subject = Subject;
+            var to = new EmailAddress(ToAddress);
+            var plainTextContent = BodyPlainText;
+            var htmlContent = BodyHtml;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+
+        public Task SendAsync(IdentityMessage message)
+        {
+            return SendGridEmailService.SendMessage(message.Destination, SendGridEmailService.DefaultFromAddress,
+                message.Subject, message.Body);
+        }
+    }
+}
